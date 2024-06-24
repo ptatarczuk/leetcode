f383b9446091f65216864a9356003293320249a4
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2024-06-24 16:01:10 +0200

    Add solution for LongestCommonPrefix

---
+++ src/main/java/com/example/leetcode/Array/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+package com.example.leetcode.Array;
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        Arrays.sort(strs);
+
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length -1].toCharArray();
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] != last[i]) {
+                break;
+            }
+            result.append(first[i]);
+
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
