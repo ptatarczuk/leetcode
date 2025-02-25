fd1ae697e9d4887bd0d46a900b7d4a927ef887a5
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-02-25 07:47:30 +0100

    Add solution for RemoveDuplicatesFromSortedArray

---
+++ src/main/java/org/example/ArrayString/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,25 @@
+package org.example.ArrayString;
+
+import java.util.Arrays;
+
+/* https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150 */
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        int p = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[p++] = nums[i];
+            }
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(Arrays.toString(nums));
+        removeDuplicates(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
