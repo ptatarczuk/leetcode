f2691e89318a2df9cec1f1c00a8086d8d0d37515
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-02-26 07:50:34 +0100

    Add solution for BestTimeToBuyAndSellStock

---
+++ src/main/java/org/example/ArrayString/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,23 @@
+package org.example.ArrayString;
+
+/* https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150 */
+
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int minPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 0;  i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            profit = Math.max(profit, prices[i] - minPrice);
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,8,4};
+
+        System.out.println(maxProfit(prices));
+    }
+}
