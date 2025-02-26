fa2e6ab021dd1a1df149e39f1857b87dd67ee838
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-02-26 09:16:00 +0100

    Add solution for RomanToInteger

---
+++ src/main/java/org/example/ArrayString/RomanToInteger.java
@@ -0,0 +1,34 @@
+package org.example.ArrayString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* https://leetcode.com/problems/roman-to-integer/?envType=study-plan-v2&envId=top-interview-150 */
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
+    }
+}
