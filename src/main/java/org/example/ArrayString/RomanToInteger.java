6002fe6a8797bf4bb63eb49eee4411d6226531d3
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-02-26 09:17:02 +0100

    Add solution for RomanToInteger using switch

--- src/main/java/org/example/ArrayString/RomanToInteger.java
+++ src/main/java/org/example/ArrayString/RomanToInteger.java
@@ -18,17 +18,58 @@ public class RomanToInteger {
         int result = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+            if (i < s.length() -1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
                 result -= map.get(s.charAt(i));
-            } else {
+                } else {
                 result += map.get(s.charAt(i));
             }
         }
         return result;
     }
 
+    /* Switch case solution, slightly better performance  https://www.youtube.com/watch?v=k4KgVd5LQk8 */
+    public static int romanToIntSwitch(String s) {
+        int sum = 0;
+        int current = translator(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            int next = translator(s.charAt(i));
+
+            if (current < next) {
+                sum -= current;
+            } else {
+                sum += current;
+            }
+            current = next;
+        }
+        sum += current;
+        return sum;
+    }
+
+    private static int translator(char c) {
+        switch(c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
     public static void main(String[] args) {
         String s = "MCMXCIV";
         System.out.println(romanToInt(s));
+        System.out.println(romanToIntSwitch(s));
     }
 }
