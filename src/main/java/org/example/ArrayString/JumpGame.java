f4ab2e6f70385d0cf5b1685c8ca59b626260204f
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-03-01 13:14:52 +0100

    Add solution for JumpGame

---
+++ src/main/java/org/example/ArrayString/JumpGame.java
@@ -0,0 +1,21 @@
+package org.example.ArrayString;
+
+/* https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150 */
+public class JumpGame {
+    public static boolean canJump(int[] nums) {
+        int finalPosition = nums.length -1;
+
+        for (int i = nums.length -2; i >= 0; i--) {
+            if (i + nums[i] >= finalPosition) {
+                finalPosition = i;
+            }
+        }
+        return finalPosition == 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,2,4};
+
+        System.out.println(canJump(nums));
+    }
+}
