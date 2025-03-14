8a36475f1a8eded9306a57c6d8391e3b0e738d2e
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-03-14 09:09:20 +0100

    Add solution for TwoSum

---
+++ src/main/java/org/example/Hashmap/HappyNumber.java
@@ -0,0 +1,41 @@
+package org.example.Hashmap;
+
+import java.util.HashSet;
+
+/* https://leetcode.com/problems/happy-number/description/?envType=study-plan-v2&envId=top-interview-150 */
+public class HappyNumber {
+    public static boolean isHappyPractice(int n) {
+        return false;
+
+    }
+    private static int sum(int num) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 13;
+
+        System.out.println(isHappy(n));
+    }
+
+    public static boolean isHappy(int n) {
+        HashSet<Integer> seenNumbers = new HashSet<>();
+
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = getSumOfSquares(n);
+        }
+        return n == 1;
+    }
+
+    private static int getSumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
--- src/main/java/org/example/Hashmap/TwoSum.java
+++ src/main/java/org/example/Hashmap/TwoSum.java
@@ -4,12 +4,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/* https://leetcode.com/problems/two-sum/description/?envType=study-plan-v2&envId=top-interview-150 */
+
 public class TwoSum {
+    public static int[] twoSumPractice(int[] nums, int target) {
+
+
+
+
+
+
+
+        return new int[]{};
+    }
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
         int target = 9;
 
         System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println(Arrays.toString(twoSumPractice(nums, target)));
 
     }
     public static int[] twoSum(int[] nums, int target) {
