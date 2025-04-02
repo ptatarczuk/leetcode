ebf66f6d66b2529140ae78fbe7368b935a93cefd
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2025-04-02 10:28:08 +0200

    Add solution for LinkedListCycle

---
+++ src/main/java/org/example/LinkedList/LinkedListCycle.java
@@ -0,0 +1,35 @@
+package org.example.LinkedList;
+
+/* https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150 */
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(0);
+        ListNode fourth = new ListNode(-4);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = second;
+
+        System.out.println(hasCycle(head));
+    }
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
